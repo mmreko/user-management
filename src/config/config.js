30d543cc2210f66640c73a092afbd862d9b6f2c5
Masha Reko <mmreko@gmail.com>
2018-08-11 12:21:34 +0200

    Changed db settings

--- src/config/config.js
+++ src/config/config.js
@@ -5,7 +5,7 @@ const dbSettings = {
 	db: 'users',
 	user: 'root',
 	pass: 'rootPassXXX',
-	server: '192.168.99.101:27017',
+	server: '192.168.99.100:27017',
 	dbParameters: () => ({
 		w: 'majority',
 		wtimeout: 10000,
